1e2156f7c1785cfd3d94b9f2070b9b8bf5134718
Veronika Romashkina <vrom911@gmail.com>
2022-06-12 11:28:48 +0100

    Upgrade copyright year

--- src/Validation.hs
+++ src/Validation.hs
@@ -7,7 +7,7 @@
 
 {- |
 Copyright:  (c) 2014 Chris Allen, Edward Kmett
-            (c) 2018-2021 Kowainik
+            (c) 2018-2022 Kowainik
 SPDX-License-Identifier: MPL-2.0
 Maintainer:  Kowainik <xrom.xkov@gmail.com>
 Stability:   Stable
